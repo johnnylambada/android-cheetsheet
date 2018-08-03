031a1c5864ad271b3d836abeb5174cb09e8419be
John Lombardo <john@lombardos.org>
2018-08-02 18:48:43 -0700

    03 Enable data binding

--- app/src/main/java/app/MainActivity.java
+++ app/src/main/java/app/MainActivity.java
@@ -1,15 +1,21 @@
 package app;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import app.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        findViewById(R.id.text).setOnClickListener(__ ->
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        binding.text.setOnClickListener(__ ->
                 Toast.makeText(this, "Click", Toast.LENGTH_LONG).show()
         );
     }
