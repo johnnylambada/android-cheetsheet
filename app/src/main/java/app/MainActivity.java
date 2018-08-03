73d1fb58c954167c11271f8b7b78f7390f578c91
John Lombardo <john@lombardos.org>
2018-08-02 18:02:36 -0700

    prep

--- app/src/main/java/app/MainActivity.java
+++ app/src/main/java/app/MainActivity.java
@@ -1,6 +1,8 @@
 package app;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
