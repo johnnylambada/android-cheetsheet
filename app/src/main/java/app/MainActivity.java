78f3173318ac00267d4f9c35b90c5b3f851343b3
John Lombardo <john@lombardos.org>
2018-08-02 18:04:21 -0700

    02 enable Java 1.8 with an example lambda expression

--- app/src/main/java/app/MainActivity.java
+++ app/src/main/java/app/MainActivity.java
@@ -2,11 +2,15 @@ package app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.text).setOnClickListener(__ ->
+                Toast.makeText(this, "Click", Toast.LENGTH_LONG).show()
+        );
     }
 }
