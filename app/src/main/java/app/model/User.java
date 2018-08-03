565bdefb62e8d6ab0d262b2ff2037222bc58a64b
John Lombardo <john@lombardos.org>
2018-08-02 22:35:14 -0700

    05 Add a recyclerview with dummy data

---
+++ app/src/main/java/app/model/User.java
@@ -0,0 +1,12 @@
+package app.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    public final String login;
+
+    public User(String login){
+        this.login = login;
+    }
+}
